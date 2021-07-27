d239e563df84f7cc8b737a2da6f3a246283febc8
Brandon <okatro350@gmail.com>
2021-07-27 13:59:45 -0500

    final

--- Fibonacci.c
+++ Fibonacci.c
@@ -5,7 +5,7 @@
 * Author: Covarrubias Minez Brandon Ivan
 * License: MIT
 *
-* GitHub: https://github.com/okatro/Programa-4-Factorial
+* GitHub: https://github.com/okatro/Programa-5
 *
 * *****************************************************************/
 
@@ -14,29 +14,50 @@
 #include <ctype.h>
 
 //Prototypes
-int isInt(char* Ap);
+long Fibonacci(int n);
+long isLong(char* Ap);
 
 int main(int argc, char** argv){
-	//Check the input, there should only be one integer and positive parameter
-	
+	//Check the input, there should only be one long and positive parameter
 	if(argc < 2){
 		printf("Usage %s p1 p2 .. pn \n",argv[0]);
 		exit(0);
 	}
 
 	for( int i = 1 ; i != argc ; i++ ){
-		if(!isInt(argv[i])){
-			printf("Usage %s p1 p2 .. pn \n",argv[0]);
+		if(!isLong(argv[i])){
+			printf("Usage %s p1 [p2] .. [pn] \n",argv[0]);
 			exit(0);
 		}
 	}
 
+	for(int i = 1 ; i != argc ; i++){
+		printf("%lu ",Fibonacci(atoi(argv[i])));
+	}
+	printf("\n");
+}
 
+long Fibonacci(int n){
+	long a;
+	long b;
 
+	switch(n){
+		case 0:
+			return 0;
+
+		case 1:
+		case 2:
+			return 1;
+
+		default:
+			a = Fibonacci(n-1);
+			b = Fibonacci(n-2);
+			return a+b;
+	};
 }
 
 //Verify that the parameter is a positive integer
-int isInt(char* Ap){
+long isLong(char* Ap){
 	while (*Ap != 0){
 		if(!isdigit(Ap[0]))
 			return 0;
